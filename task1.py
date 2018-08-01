61b757ebb20d5da9f52181f4c912285cf94d84ad
Winnieshirow <shirowwinnie@gmail.com>
2018-08-01 17:07:39 +0300

    gg

--- task1.py
+++ task1.py
@@ -1,9 +1,13 @@
 import random 
 
 ourList = list()
+belowFive = list()
 count = 0 
 while (count < 11):
     ourList.append(random.randint(1,10))
     count += 1
+    if ourList[count-1] < 5:
+        belowFive.append(ourList[count-1])
     
-ourList
+print(ourList)
+print(belowFive)
--- task2.py
+++ task2.py
@@ -1,6 +1,11 @@
 import random
+import array
+reversePhrase = list()
 
 words = [word.rstrip('\n') for word in open('words.txt')]
 randomPhrase = " ".join([words[random.randrange(0, len(words))] for i in range(4)])
 
-randomPhrase
+reversePhrase = randomPhrase[::-1]
+
+print(randomPhrase)
+print(reversePhrase)
